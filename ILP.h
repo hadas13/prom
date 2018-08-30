e0841566fc2c2cd2f238941f231222d6b32044e2
omergrin93 <41589764+omergrin93@users.noreply.github.com>
2018-08-30 17:47:01 +0300

    ILP header and source
    ILP that supports the hint (return cell's value if success or 0 otherwise), generate (fills the given board and return 1 if success, 0 otherwise) and validate (returns binary) commands

---
+++ ILP.h
@@ -0,0 +1,16 @@
+/*
+ * ILP.h
+ *
+ *  Created on: 29 баев„ 2018
+ *      Author: Omer
+ */
+
+#ifndef ILP_H_
+#define ILP_H_
+
+#include "defines.h"
+
+int run_ILP(Board *board, int command_index, int row, int col);
+
+
+#endif /* ILP_H_ */
