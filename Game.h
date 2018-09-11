b3042dcfd149b44c28b93735e07d3320235bfb0c
hadas13 <41589439+hadas13@users.noreply.github.com>
2018-09-12 01:15:06 +0300

    compiling with reset

--- Game.h
+++ Game.h
@@ -1,16 +1,22 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+#include <stdio.h>
+#include <string.h>
+
 #include "defines.h"
 #include "Parser.h"
 #include "Solver.h"
 #include "GUI.h"
 #include "Moves.h"
-#include "MainAux.h"
 #include "Backtracking.h"
+#include "ILP.h"
+#include "MainAux.h"
+
 
 #define MAX_ITER_GENERATE 1000 /* iterations num of generating x numbers in generate command */
 #define NOT_POSSIBLE_VAL -1
+#define FINISHED_UNDO     3
 
 /****** functions ********/
 /*
@@ -18,14 +24,6 @@
  * and prints the a line of "-" that frames the board
  */
 void print_frame(Board *board);
-
-/*
- * Function that receives a Command structure, checks if its valid
- * and handles the different commands types according to the instructions
- */
-
-
-/*void turn(struct Command command, Board *board, Game *game);*/
 
 
 /* 
@@ -44,6 +42,7 @@ int init_game_table(Board *board);
  * @param fixed_nums: number of fixed cells.
  * @return value: new game board and solution. 
  */
+
 Board *init_board(int n, int m_rows, int m_cols, int fixed_nums);
 
 Game init_game();
@@ -57,7 +56,7 @@ int play_mark_errors(Game *game, int is_mark);
  * or asked to exit, and 0 if the current games needs to continue
  */
 
-int stop_game(Board *board);
+int stop_game(Board *board); /* TODO delete */
 
 /*
  * Function that manages playing the game
@@ -103,6 +102,29 @@ int play_num_solutions(Board *board);
 int play_generate(Game *game, Board *board, int x, int y);
 
 int free_game(Game *game);
+
+/*
+ * function that receives a path to a sudoku file and a board and reads the sudoku from the file to the board
+ */
+int read_sudoku(char *path, Board *board);
+
+/*
+ * function the receives a board and a file path and loads the sudoku in the file to play in solve mode.
+ * return 1 on success and 0 otherwise
+ */
+int play_solve(Board *board, char *path, Game *game);
+
+/*
+ * function the receives a board and an optional file path and loads the sudoku in the file to play in edit mode.
+ * return 1 on success and 0 otherwise
+ */
+int play_edit(Board *board, char *path, Game *game);
+
+
+/*
+ * function the frees all allocated memory and exits the game
+ */
+int play_exit(Board *board, Game *game);
 
 int play_undo(Board *board, Game *game);
 
--- Moves.h
+++ Moves.h
@@ -6,25 +6,21 @@
 #include <stdlib.h>
 
 /***** consts for moves chain *********/
+
+#define EMPTY_VALUE_FOR_INIT -1
 #define TRUE       1
 #define FALSE      0
 #define VALID      1
 #define NOT_VALID  0
 #define UNASSIGNED 0
-
-#define PROBBLY_IN_LOOP       20000
-#define EMPTY_VALUE_FOR_INIT -1
-#define FINISHED_UNDO           3
-
+#define PROBBLY_IN_LOOP      20000
 
 /***** functions **********/
+
 /* checking if the move is fixed value, if not - inserting the new move to new_move */
 int create_new_move(Board *board, MoveInfo *new_move, int row, int column, int value);
 
 /*
- * from @param begining removes all next moves in the linked list */
-int remove_next_moves(MoveList *begining);
-/* 
  * @param curr_move: the current move in the game that we are playing in.
  * @param new_move: the new information for the new move that the function will add to the new
  *		    move list.
@@ -34,7 +30,6 @@ int append_new_move_to_moves_list(MoveList *curr_move, MoveInfo new_move);
 
 /* create chain of moves that connected to generate move */
 /*int create_generate_chain(Board *game_board, Board *generate_board, MoveInfo move);*/
-
 /*
  * @param **chain: the main chain of moves that going to be hold all the changes that
  *		   going to be made in autofill.
@@ -44,6 +39,8 @@ int create_autofill_chain(MoveList **chain);
 
 /* finds the begining of chain, and removes all the moves */
 int remove_moves_from_begining(MoveList **chain);
+
+int remove_next_moves(MoveList *begining);
 
 /* makes undo on board */
 int undo_on_board(Board *board, MoveInfo current);
